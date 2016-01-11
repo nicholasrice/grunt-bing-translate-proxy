fe19ecaac6571b36d2c6311d65daa30c8cafe71f
Nick Rice <nicholas.rice119@gmail.com>
2016-01-10 17:36:20 -0800

    adding functions to create and check existance of valid access tokens

--- tasks/bing_translate_proxy.js
+++ tasks/bing_translate_proxy.js
@@ -8,28 +8,112 @@
 
 'use strict';
 
-var chalk = require('chalk');
+var chalk = require('chalk'),
+    fs = require('fs'),
+    http = require('http'),
+    https = require('https'),
+    querystring = require('querystring');
+
 
 module.exports = function(grunt) {
+    grunt.registerMultiTask('bing_translate_proxy', 'Local proxy server to request translations from using Bing Translate API', function() {
+        var done = this.async(); // Tell grunt to keep task running until canceled
+        var tempStorage = ".grunt/grunt-bing-translate-proxy/";
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+        //--------------------------------------------------------------------------
+        // Define config requirements and defaults
+        //--------------------------------------------------------------------------
+        var options = this.options();
+        options.protocol = options.protocol || 'http:';
+        options.domain = options.domain || 'localhost';
+        options.port = options.port || 8080;
 
-  grunt.registerMultiTask('bing_translate_proxy', 'Local proxy server to request translations from using Bing Translate API', function() {
+        if (options.client_id === undefined) {
+            return new Error(chalk.red("client_id is not defined"));
+        }
 
-    var options = this.options();
-    options.protocol = options.protocol || 'http:';
-    options.domain = options.domain || 'localhost';
-    options.port = options.port || 8080;
+        if (options.client_secret === undefined) {
+            return new Error(chalk.red("client_secret is not defined"));
+        }
 
-    if (options.client_id === undefined) {
-      return new Error(chalk.red("client_id is not defined"));
-    }
+        //--------------------------------------------------------------------------
+        // Proxy entry function
+        //--------------------------------------------------------------------------
+        function handleRequest(request, response) {
+            if (! validAccessToken()) {
+                requestTranslateAccessToken();
+            }
 
-    if (options.client_secret === undefined) {
-      return new Error(chalk.red("client_secret is not defined"));
-    }
+        }
 
-    
-  });
+        //--------------------------------------------------------------------------
+        // Determine if access token exists and is valid
+        //--------------------------------------------------------------------------
+        function validAccessToken() {
+            if (grunt.file.exists(tempStorage + "translate-access-token.json")) {
+                var token = grunt.file.readJSON(tempStorage + "translate-access-token.json"),
+                    now = Date.now(),
+                    requestDate = Date.parse(token.timestamp);
+
+                    return now < requestDate + (token.expires_in * 1000);
+
+            } else {
+                return false;
+            }
+        }
+        //--------------------------------------------------------------------------
+        // Bing API token request function.
+        //--------------------------------------------------------------------------
+        function requestTranslateAccessToken() {
+            var post_data = querystring.stringify({
+                "client_id": options.client_id,
+                "client_secret": options.client_secret,
+                "scope": "http://api.microsofttranslator.com",
+                "grant_type": "client_credentials"
+            });
+
+            var post_options = {
+                "protocol": "https:",
+                "host": "datamarket.accesscontrol.windows.net",
+                "path": "/v2/OAuth2-13",
+                "method": "POST",
+                "headers": {
+                    'Content-Length': Buffer.byteLength(post_data)
+                }
+            };
+
+            var timestamp = new Date();
+            var post_req = https.request(post_options, function(res) {
+                res.setEncoding('utf8');
+                res.on('data', function(chunk) {
+                    var token = JSON.parse(chunk);
+                    token.timestamp = timestamp;
+                    grunt.file.write(
+                        tempStorage + "translate-access-token.json",
+                        JSON.stringify(token)
+                    );
+                });
+            });
+
+            post_req.write(post_data);
+            post_req.end();
+        }
+
+        //--------------------------------------------------------------------------
+        // Proxy Server Setup
+        //--------------------------------------------------------------------------
+        var PORT = options.port;
+        var server = http.createServer(handleRequest);
+
+        //--------------------------------------------------------------------------
+        // Start server
+        //--------------------------------------------------------------------------
+        server.listen(PORT, function() {
+          console.log("Server listening on: http://localhost:%s", PORT);
+        });
+
+        if (! validAccessToken()) {
+            requestTranslateAccessToken();
+        }
+    });
 };
