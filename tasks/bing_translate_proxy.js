1724114aa845c96f76de68ccade8981b02a9365d
Nick Rice <nicholas.rice119@gmail.com>
2016-01-10 22:38:25 -0800

    response wired up

--- tasks/bing_translate_proxy.js
+++ tasks/bing_translate_proxy.js
@@ -12,13 +12,15 @@ var chalk = require('chalk'),
     fs = require('fs'),
     http = require('http'),
     https = require('https'),
-    querystring = require('querystring');
+    querystring = require('querystring'),
+    xml2js = require('xml2js');
 
 
 module.exports = function(grunt) {
     grunt.registerMultiTask('bing_translate_proxy', 'Local proxy server to request translations from using Bing Translate API', function() {
         var done = this.async(); // Tell grunt to keep task running until canceled
         var tempStorage = ".grunt/grunt-bing-translate-proxy/";
+        var parser = new xml2js.Parser();
 
         //--------------------------------------------------------------------------
         // Define config requirements and defaults
@@ -44,8 +46,54 @@ module.exports = function(grunt) {
                 requestTranslateAccessToken();
             }
 
+            console.log(request);
+            
+            var text = "Today I went to the store to buy groceries",
+                from = "en",
+                to = "de";
+
+            requestTranslation(text, from, to, function(translation) {
+                response.statusCode = 200; // TODO is this true?
+                response.write(translation);
+                response.end();
+            });
         }
 
+        function requestTranslation(text, from, to, callback) {
+            // TODO make sure we have all of these arguments
+            var path = querystring.stringify({
+                "text": text,
+                "from": encodeURIComponent(from),
+                "to": encodeURIComponent(to)
+            });
+
+            var authToken = "Bearer " + grunt.file.readJSON(tempStorage + "translate-access-token.json").access_token;
+
+            var req_options = {
+                protocol: 'http:',
+                host: 'api.microsofttranslator.com',
+                path: "/v2/Http.svc/Translate?" + path,
+                method: "GET",
+                headers: {
+                    "Authorization": authToken
+                }
+            };
+
+            var request = http.request(req_options, function(res) {
+                res.setEncoding('utf8');
+                res.on('data', function(chunk) {
+                    parser.parseString(chunk, function(err, data) {
+                        if (err) {
+                            return new Error(err);
+                        }
+
+                        callback(data.string._);
+                    });
+                });
+            });
+
+            request.end();
+        }
         //--------------------------------------------------------------------------
         // Determine if access token exists and is valid
         //--------------------------------------------------------------------------
@@ -64,7 +112,7 @@ module.exports = function(grunt) {
         //--------------------------------------------------------------------------
         // Bing API token request function.
         //--------------------------------------------------------------------------
-        function requestTranslateAccessToken() {
+        function requestTranslateAccessToken(callback) {
             var post_data = querystring.stringify({
                 "client_id": options.client_id,
                 "client_secret": options.client_secret,
@@ -92,6 +140,10 @@ module.exports = function(grunt) {
                         tempStorage + "translate-access-token.json",
                         JSON.stringify(token)
                     );
+
+                    if (callback !== undefined) {
+                        callback();
+                    }
                 });
             });
 
@@ -109,7 +161,7 @@ module.exports = function(grunt) {
         // Start server
         //--------------------------------------------------------------------------
         server.listen(PORT, function() {
-          console.log("Server listening on: http://localhost:%s", PORT);
+          console.log("Server listening on: http://localhost:%s", PORT); // TODO lets make this more relevant
         });
 
         if (! validAccessToken()) {
