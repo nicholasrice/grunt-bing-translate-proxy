6201d786b5156eddebc87c0e37796ee1c148ad46
Nick Rice <nicholas.rice119@gmail.com>
2016-01-12 22:42:37 -0800

    updating test to test for statusCode

--- test/bing_translate_proxy_test.js
+++ test/bing_translate_proxy_test.js
@@ -2,6 +2,11 @@
 
 var grunt = require('grunt');
 var http = require('http');
+var querystring = require('querystring');
+
+var text = 'Testing is fun!';
+var from = 'en';
+var to = 'de'
 /*
   ======== A Handy Little Nodeunit Reference ========
   https://github.com/caolan/nodeunit
@@ -24,16 +29,26 @@ var http = require('http');
 
 exports.bing_translate_proxy = {
     setUp: function(done) {
-        // setup here if necessary
+
         done();
     },
     default_options: function(test) {
-        test.expect(1);
+        test.expect(2);
 
-        http.get('http://localhost:8080', function(res) {
+        var path = querystring.stringify({
+            to: to,
+            from: from,
+            text: text
+        });
+
+        http.get('http://localhost:8080?' + path, function(res) {
             res.setEncoding('utf8');
+
+            test.strictEqual(res.statusCode, 200, 'Status code should be 200');
+
             res.on('data', function(chunk) {
                 test.strictEqual(typeof chunk, 'string', 'Result should be a string');
+                console.log(chunk);
                 test.done();
             });
         }).on('error', function(e) {
