b21fd65e75695f643f6b341f4b9cf9c230c8bb69
Nick Rice <v-nichor@microsoft.com>
2016-04-20 16:38:21 -0700

    updating gruntfile with options

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,62 +10,67 @@
 
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        'tasks/*.js',
-        // '<%= nodeunit.tests %>'
-      ],
-      options: {
-        jshintrc: '.jshintrc'
-      }
-    },
+    // Project configuration.
+    grunt.initConfig({
+        jshint: {
+            all: [
+                'Gruntfile.js',
+                'tasks/*.js',
+                // '<%= nodeunit.tests %>'
+            ],
+            options: {
+                jshintrc: '.jshintrc'
+            }
+        },
 
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['tmp']
-    },
+        // Before generating any new files, remove any previously-created files.
+        clean: {
+            tests: ['tmp']
+        },
 
-    watch: {
-        default: {
-            files: ['**/*.js'],
-            tasks: ['default']
+        watch: {
+            default: {
+                files: ['**/*.js'],
+                tasks: ['default']
+            }
+        },
+
+        // Configuration to be run (and then tested).
+        bing_translate_proxy: {
+            default_options: {
+                options: {
+                    auth_key: "key"
+                }
+            },
+            custom_auth_file: {
+                options: {
+                    auth_location: 'custom-auth.json',
+                    auth_key: "custom_key"
+                }
+            }
+        },
+
+        // Unit tests.
+        nodeunit: {
+            tests: ['test/*_test.js']
         }
-    },
 
-    // Configuration to be run (and then tested).
-    bing_translate_proxy: {
-      default_options: {
-        options: {
-          client_id: '[client_id]',
-          client_secret: '[client_secret]'
-        }
-      }
-    },
+    });
 
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js']
-    }
+    // Actually load this plugin's task(s).
+    grunt.loadTasks('tasks');
 
-  });
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-nodeunit');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
+    // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+    // plugin's task(s), then test the result.
+    grunt.registerTask('test', ['clean', 'bing_translate_proxy', 'nodeunit']);
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'bing_translate_proxy', 'nodeunit']);
-
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test', 'watch']);
+    // By default, lint and run all tests.
+    grunt.registerTask('default', ['jshint', 'test', 'watch']);
 
 };
